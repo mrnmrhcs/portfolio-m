ea8485e41e22ea128e13ffba363d4bcd8710636c
Steffen Dietz <sd@dietaikonauten.com>
2016-06-19 19:39:27 +0200

    this starts the routing - but still not sure how to do it

--- js/ms.js
+++ js/ms.js
@@ -3,6 +3,8 @@ console.log('######## MS ########');
 var menuTrigger = document.getElementById('menu');
 var header = document.querySelector('header');
 
+var navs = document.querySelectorAll('[data-route]');
+
 menuTrigger.addEventListener('click', function(){
 
   if(header.classList.contains('is-active')) {
