7dfa54f43d790867782da792d2ae4a9e1147a8d3
Booozz <blody.ti@googlemail.com>
2016-06-22 00:57:33 +0200

    fix the integration of npm dependencies, update gulpfile, add grid classes in html

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var gulp = require('gulp');
+var gnf = require('gulp-npm-files');
+var rename = require("gulp-rename");
 var sass = require('gulp-sass');
 var cssnano = require('gulp-cssnano');
 var sourcemaps = require('gulp-sourcemaps');
@@ -19,8 +21,20 @@ var browserSync = require('browser-sync').create();
 gulp.task('browserSync', function() {
     browserSync.init({
         server: "./src",
-    })
-})
+    });
+});
+gulp.task('modules:get', function() {
+    return gulp.src(gnf(), {base:'./'})
+    .pipe(gulpIf('*.css', gulp.dest('./src/css/')));
+});
+gulp.task('modules:build', function() {
+    return gulp.src("./src/css/node_modules/**/*.css")
+    .pipe(rename(function (path) { path.extname = ".css";}))
+    .pipe(gulp.dest("./src/css/modules"));
+});
+gulp.task('modules', function (callback) {
+    return runSequence('clean:modules','modules:get','modules:build','clean:node_modules',callback);
+});
 gulp.task('sass', function () {
     gulp.src('./src/sass/**/*.scss')
     .pipe(sourcemaps.init())
@@ -34,17 +48,20 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./src/css/'))
     .pipe(browserSync.reload({
         stream: true
-    }))
+    }));
 });
 gulp.task('watch', ['browserSync', 'sass'], function () {
-    gulp.watch('./src/sass/**/*.scss', ['sass']);
-    gulp.watch('./src/*.html', browserSync.reload);
-    gulp.watch('./src/pages/*.html', browserSync.reload);
-    gulp.watch('./src/js/**/*.js', browserSync.reload);
+    gulp.watch('./src/sass/**/*.scss', ['sass'])
+    gulp.watch('./src/*.html', browserSync.reload)
+    gulp.watch('./src/pages/*.html', browserSync.reload)
+    gulp.watch('./src/js/**/*.js', browserSync.reload)
 });
 
 //  production
 
+gulp.task('clear:cache', function (callback) {
+    return cache.clearAll(callback);
+});
 gulp.task('useref', function(){
     return gulp.src(['./src/pages/*.html','./src/index.html'])
     .pipe(useref())
@@ -52,39 +69,38 @@ gulp.task('useref', function(){
     .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulpIf('!*.html', gulp.dest('./dist')))
     .pipe(gulpIf(['*.html','!index.html'], gulp.dest('./dist/pages')))
-    .pipe(gulpIf('index.html', gulp.dest('./dist')))
+    .pipe(gulpIf('index.html', gulp.dest('./dist')));
 });
 gulp.task('images', function(){
     return gulp.src('./src/assets/images/*.+(png|jpg|gif)')
     .pipe(cache(imagemin({
         interlaced: true
     })))
-    .pipe(gulp.dest('dist/assets/images'))
+    .pipe(gulp.dest('dist/assets/images'));
 });
 gulp.task('icons', function() {
     return gulp.src('./src/assets/icons/*')
-    .pipe(gulp.dest('dist/assets/icons'))
-})
+    .pipe(gulp.dest('dist/assets/icons'));
+});
 gulp.task('fonts', function() {
     return gulp.src('./src/assets/fonts/**/*')
-    .pipe(gulp.dest('dist/assets/fonts'))
-})
+    .pipe(gulp.dest('dist/assets/fonts'));
+});
 gulp.task('clean:dist', function() {
     return del.sync('dist');
-})
-gulp.task('cache:clear', function (callback) {
-    return cache.clearAll(callback)
-})
+});
+gulp.task('clean:modules', function() {
+    return del.sync('./src/css/lib');
+});
+gulp.task('clean:node_modules', function() {
+    del.sync('./src/css/node_modules');
+});
 
 //  sequences
 
 gulp.task('default', function (callback) {
-    runSequence(['sass','browserSync', 'watch'],
-    callback
-    )
-})
+    runSequence('modules',['sass','browserSync','watch'],callback);
+});
 gulp.task('build', function (callback) {
-    runSequence('clean:dist', ['sass', 'useref', 'images', 'icons', 'fonts'],
-    callback
-    )
-})
+    runSequence('clean:dist','modules',['sass','useref','images','icons','fonts'],callback);
+});
