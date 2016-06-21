5f1086b735b4df800f74ef196e92808c8c3cbca1
Booozz <blody.ti@googlemail.com>
2016-06-21 02:22:20 +0200

    cleanup gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -10,11 +10,11 @@ var gulpIf = require('gulp-if');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 var cache = require('gulp-cache');
-var browserSync = require('browser-sync').create();
 var del = require('del');
 var runSequence = require('run-sequence');
+var browserSync = require('browser-sync').create();
 
-//  development process
+//  development
 
 gulp.task('browserSync', function() {
     browserSync.init({
@@ -26,40 +26,39 @@ gulp.task('sass', function () {
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
+        browsers: ['last 2 versions'],
+        cascade: false
     }))
     .pipe(cssnano())
     .pipe(sourcemaps.write('./'))
-
-  .pipe(gulp.dest('./src/css/'))
-  .pipe(browserSync.reload({
-      stream: true
+    .pipe(gulp.dest('./src/css/'))
+    .pipe(browserSync.reload({
+        stream: true
     }))
 });
 gulp.task('watch', ['browserSync', 'sass'], function () {
-  gulp.watch('./src/sass/**/*.scss', ['sass']);
-  gulp.watch('./src/*.html', browserSync.reload);
-  gulp.watch('./src/pages/*.html', browserSync.reload);
-  gulp.watch('./src/js/**/*.js', browserSync.reload);
+    gulp.watch('./src/sass/**/*.scss', ['sass']);
+    gulp.watch('./src/*.html', browserSync.reload);
+    gulp.watch('./src/pages/*.html', browserSync.reload);
+    gulp.watch('./src/js/**/*.js', browserSync.reload);
 });
 
-//  production process
+//  production
 
 gulp.task('useref', function(){
-  return gulp.src(['./src/pages/*.html','./src/index.html'])
-  .pipe(useref())
-  .pipe(gulpIf('*.js', uglify()))
-  .pipe(gulpIf('*.css', cssnano()))
-  .pipe(gulpIf('!*.html', gulp.dest('./dist')))
-  .pipe(gulpIf(['*.html','!index.html'], gulp.dest('./dist/pages')))
-  .pipe(gulpIf('index.html', gulp.dest('./dist')))
+    return gulp.src(['./src/pages/*.html','./src/index.html'])
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulpIf('!*.html', gulp.dest('./dist')))
+    .pipe(gulpIf(['*.html','!index.html'], gulp.dest('./dist/pages')))
+    .pipe(gulpIf('index.html', gulp.dest('./dist')))
 });
 gulp.task('images', function(){
     return gulp.src('./src/assets/images/*.+(png|jpg|gif)')
-        .pipe(cache(imagemin({
-            interlaced: true
-        })))
+    .pipe(cache(imagemin({
+        interlaced: true
+    })))
     .pipe(gulp.dest('dist/assets/images'))
 });
 gulp.task('icons', function() {
@@ -77,7 +76,7 @@ gulp.task('cache:clear', function (callback) {
     return cache.clearAll(callback)
 })
 
-//  task sequences
+//  sequences
 
 gulp.task('default', function (callback) {
     runSequence(['sass','browserSync', 'watch'],
