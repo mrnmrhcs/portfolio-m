224f269bde4f5e134dc3b88aeeb6054a42caa01e
mrnschrmm <>
2019-11-24 07:12:04 +0100

    update(): content

--- app/templates/about.php
+++ app/templates/about.php
@@ -1,39 +1,48 @@
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/snippets/document/header/index.php'; ?>
 
 <div class="snippet" is="layout-about">
-  <div class="about context-content">
+  <div class="about">
     <section class="about__layout">
-      <header class="about__header">
-        <h1 class="about__title">about</h1>
-        <p class="about__intro">Hello — I‘m Marian, a creative front-end developer with an understanding for visual and ux design, who values hard work, critical thinking and a methodical workflow to build digital products in a clean and usable manner.</p>
+      <header class="about__header context-content">
+        <div class="container">
+          <h1 class="about__title">Hello</h1><span class="about__divider">—</span>
+          <p class="about__intro">I’m a web developer with a good sense for ux and interface design, who values hard work and a methodical workflow to build digital products in a clean and usable manner.</p>
+        </div>
       </header>
       <main class="about__main">
-        <div class="about__experience">
-          <h2>Work Experience</h2>
-          <p>Graduated from Mediadesign Hochschule Berlin in 2008, i have worked in design &amp; web development agencies in germany to help in a variety of successful digital product and experience projects, both small and large companies.</p>
-          <p>I'm a self-motivated and creative front-end developer with a great enthusiasm for all technology related aspects of digital ecosystems.</p>
-        </div>
-        <div class="about__skills">
-          <div class="about__skill">
-            <h3>Development Experience</h3>
-            <ul>
-              <li>HTML5 / CSS3</li>
-              <li>Javascript</li>
-              <li>Kirby CMS Development</li>
-              <li>Git Version Control</li>
-              <li>Gulp Task Automation</li>
-            </ul>
-          </div>
-          <div class="about__skill">
-            <h3>Design Experience</h3>
-            <ul>
-              <li>User Experience Design</li>
-              <li>UI &amp; Visual Design</li>
-              <li>Prototypes &amp; Wireframes</li>
-              <li>Responsive Design</li>
-            </ul>
+        <div class="about__experience context-content">
+          <div class="container">
+            <h2>Work Experience</h2>
+            <p>Graduated from Mediadesign Hochschule Berlin in 2008, i have worked in design &amp; web development agencies in germany to help in a variety of successful digital product and experience projects, both small and large companies.</p>
+            <p>I'm a self-motivated and creative front-end developer with a great enthusiasm for all technology related aspects of digital ecosystems.</p>
           </div>
         </div>
+        <div class="about__skills context-content">
+          <div class="container">
+            <div class="about__skill">
+              <h3>Development Experience</h3>
+              <ul>
+                <li>HTML5 / CSS3</li>
+                <li>Javascript</li>
+                <li>Kirby CMS Development</li>
+                <li>Git Version Control</li>
+                <li>Gulp Task Automation</li>
+              </ul>
+            </div>
+            <div class="about__skill">
+              <h3>Design Experience</h3>
+              <ul>
+                <li>User Experience Design</li>
+                <li>UI &amp; Visual Design</li>
+                <li>Prototypes &amp; Wireframes</li>
+                <li>Responsive Design</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!-- <div class="about__contribution">
+            <?php include $_SERVER['DOCUMENT_ROOT'] . '/snippets/component/block-graph-contribution/index.php'; ?>
+        </div> -->
       </main>
     </section>
   </div>
