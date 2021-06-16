dcce236ba0df8b366f3b88b65448074ccefe7e37
mrnmrhcs <m.service@kaimberg.com>
2021-06-16 03:51:08 +0200

    update(gulp): refactor

--- gulpfile.esm.js
+++ gulpfile.esm.js
@@ -23,13 +23,6 @@ import config from './config'
 const ENV = process.env.NODE_ENV
 const DEBUG = (process.env.NODE_DEBUG) ? true : false
 
-// PATHS
-const path = prep(config.path)
-const root_src = path.root_src
-const root_dist = path.root_dist
-const resources = path.resources
-
-// INFO
 console.log('ENV:', ENV)
 console.log('DEBUG:', DEBUG)
 
@@ -66,12 +59,10 @@ function reload (done) {
 // VENDOR
 ////////////////////////////////////////////////////////////////////////////////
 
-const vendor = series(clean__vendor, process__vendor_head, process__vendor)
-
 // CLEAN -------------------------------------------------------------
 
 function clean__vendor () {
-  return del([config.vendor.dest + '{vendor.head,vendor}.min.js'])
+  return del(config.vendor.dest + '{vendor.head,vendor}.min.js')
 }
 
 // PROCESS -------------------------------------------------------------
@@ -94,24 +85,27 @@ function process__vendor () {
     .pipe(dest(config.vendor.dest))
 }
 
+// COMPOSITION -------------------------------------------------------------
+
+const vendor = series(clean__vendor, process__vendor_head, process__vendor)
+
 ////////////////////////////////////////////////////////////////////////////////
 // SEO
 ////////////////////////////////////////////////////////////////////////////////
 
-const seo__src = (root_src).replace('//', '/')
-const seo__dest = (root_dist).replace('//', '/')
-
 // CLEAN -------------------------------------------------------------
 
-function clean__robots () { return del([seo__dest + 'robots.txt']) }
+function clean__robots () {
+  return del(config.path.dist + 'robots.txt')
+}
 
 // COPY -------------------------------------------------------------
 
 function copy__robots () {
-  return src([seo__src + (!ENV === 'staging' ? 'robots.txt' : 'robots.txt')])
+  return src(config.path.src + (!ENV === 'staging' ? 'robots.txt' : 'robots.txt'))
     .pipe(gulpif(DEBUG, debug({ title: '## ROBOTS:' })))
     .pipe(rename('robots.txt'))
-    .pipe(dest(seo__dest))
+    .pipe(dest(config.path.dist))
 }
 
 // COMPOSITION -------------------------------------------------------------
@@ -122,55 +116,46 @@ const robots = series(clean__robots, copy__robots)
 // LOGIC
 ////////////////////////////////////////////////////////////////////////////////
 
-const index__src = (root_src).replace('//', '/')
-const index__dest = (root_dist).replace('//', '/')
-
-const snippets__src = (root_src + path.snippets).replace('//', '/')
-const snippets__dest = (root_dist + path.snippets).replace('//', '/')
-
-const templates__src = (root_src + path.templates).replace('//', '/')
-const templates__dest = (root_dist + path.templates).replace('//', '/')
-
 // CLEAN -------------------------------------------------------------
 
-function clean__index () { return del([index__dest + 'index.php']) }
-function clean__htaccess () { return del([index__dest + '.htaccess']) }
-function clean__snippets () { return del([snippets__dest]) }
-function clean__templates () { return del([templates__dest]) }
+function clean__index () { return del(config.path.dist + 'index.php') }
+function clean__htaccess () { return del(config.path.dist + '.htaccess') }
+function clean__snippets () { return del(config.path.dist + config.path.snippets) }
+function clean__templates () { return del(config.path.dist + config.path.templates) }
 
 // COPY -------------------------------------------------------------
 
 function copy__htaccess () {
-  return src([index__src + '.htaccess'])
+  return src(config.path.src + '.htaccess')
     .pipe(gulpif(DEBUG, debug({ title: '## HTACCESS:' })))
-    .pipe(dest(index__dest))
+    .pipe(dest(config.path.dist))
 }
 
 function copy__index () {
-  return src([index__src + 'index.php'])
+  return src(config.path.src + 'index.php')
     .pipe(gulpif(DEBUG, debug({ title: '## INDEX:' })))
-    .pipe(dest(index__dest))
+    .pipe(dest(config.path.dist))
 }
 
 function copy__snippets () {
-  return src([snippets__src + '**/*.php'])
+  return src(config.path.src + config.path.snippets + '**/*.php')
     .pipe(gulpif(DEBUG, debug({ title: '## SNIPPETS:' })))
-    .pipe(dest(snippets__dest))
+    .pipe(dest(config.path.dist + config.path.snippets))
 }
 
 function copy__templates () {
-  return src([templates__src + '**/*.php'])
+  return src(config.path.src + config.path.templates + '**/*.php')
     .pipe(gulpif(DEBUG, debug({ title: '## TEMPLATES:' })))
-    .pipe(dest(templates__dest))
+    .pipe(dest(config.path.dist + config.path.templates))
 }
 
 // WATCH -------------------------------------------------------------
 
 function watch__logic () {
-  watch(index__src + 'index.php', series(index, reload))
-  watch(index__src + '.htaccess', series(htaccess, reload))
-  watch(snippets__src + '**/*.php', series(snippets, reload))
-  watch(templates__src + '**/*.php', series(templates, reload))
+  watch(config.path.src + 'index.php', series(index, reload))
+  watch(config.path.src + '.htaccess', series(htaccess, reload))
+  watch(config.path.src + config.path.snippets + '**/*.php', series(snippets, reload))
+  watch(config.path.src + config.path.templates + '**/*.php', series(templates, reload))
 }
 
 // COMPOSITION -------------------------------------------------------------
@@ -184,40 +169,28 @@ const templates = series(clean__templates, copy__templates)
 // ASSETS
 ////////////////////////////////////////////////////////////////////////////////
 
-const assets__images__src = (root_src + resources + path.assets + path.images).replace('//', '/')
-const assets__images__dest = (root_dist + path.assets + path.images).replace('//', '/')
-
-const assets__icons__src = (root_src + resources + path.assets + path.icons).replace('//', '/')
-const assets__icons__dest = (root_dist + path.assets + path.icons).replace('//', '/')
-
-const assets__favicons__src = (root_src + resources + path.assets + path.favicons).replace('//', '/')
-const assets__favicons__dest = (root_dist + path.assets + path.favicons).replace('//', '/')
-
-const assets__fonts__src = (root_src + resources + path.assets + path.fonts).replace('//', '/')
-const assets__fonts__dest = (root_dist + path.assets + path.fonts).replace('//', '/')
-
 // CLEAN -------------------------------------------------------------
 
-function clean__images () { return del([assets__images__dest]) }
-function clean__icons () { return del([assets__icons__dest]) }
-function clean__favicons () { return del([assets__favicons__dest]) }
-function clean__fonts () { return del([assets__fonts__dest]) }
+function clean__images () { return del(config.path.dist + config.path.assets + config.path.images) }
+function clean__icons () { return del(config.path.dist + config.path.assets + config.path.icons) }
+function clean__favicons () { return del(config.path.dist + config.path.assets + config.path.favicons) }
+function clean__fonts () { return del(config.path.dist + config.path.assets + config.path.fonts) }
 
 // PROCESS -------------------------------------------------------------
 
 function process__images () {
-  return src(assets__images__src + '**/*.{png,jpg,jpeg,gif}')
+  return src(config.path.src + config.path.resources + config.path.assets + config.path.images + '**/*.{png,jpg,jpeg,gif}')
     .pipe(debug({ title: '## IMAGES:' }))
     .pipe(cache(imagemin([
       imagemin.gifsicle({ interlaced: true }),
       imagemin.mozjpeg({ quality: 75, progressive: true }),
       imagemin.optipng({ optimizationLevel: 7 })
     ])))
-    .pipe(dest(assets__images__dest))
+    .pipe(dest(config.path.dist + config.path.assets + config.path.images))
 }
 
 function process__icons () {
-  return src(assets__icons__src + '**/*.svg')
+  return src(config.path.src + config.path.resources + config.path.assets + config.path.icons + '**/*.svg')
     .pipe(debug({ title: '## ICONS:' }))
     .pipe(cache(imagemin([
       imagemin.svgo({
@@ -230,14 +203,14 @@ function process__icons () {
         verbose: DEBUG ? true : false
       })
     ])))
-    .pipe(dest(assets__icons__dest))
+    .pipe(dest(config.path.dist + config.path.assets + config.path.icons))
 }
 
 function process__favicons () {
-  return src([assets__favicons__src + 'favicon_src.png'])
+  return src(config.path.src + config.path.resources + config.path.assets + config.path.favicons + 'favicon_src.png')
     .pipe(favicon({
       background: '#FFFFFF',
-      path: assets__favicons__dest,
+      path: config.path.dist + config.path.assets + config.path.favicons,
       url: config.host.live,
       display: 'standalone',
       orientation: 'portrait',
@@ -256,22 +229,22 @@ function process__favicons () {
       }
     }))
     .pipe(gulpif(DEBUG, debug({ title: '## FAVICON:' })))
-    .pipe(dest(assets__favicons__dest))
+    .pipe(dest(config.path.dist + config.path.assets + config.path.favicons))
 }
 
 function copy__fonts () {
-  return src([assets__fonts__src + '**/*.{woff,woff2}'])
+  return src(config.path.src + config.path.resources + config.path.assets + config.path.fonts + '**/*.{woff,woff2}')
     .pipe(gulpif(DEBUG, debug({ title: '## FONTS:' })))
-    .pipe(dest(assets__fonts__dest))
+    .pipe(dest(config.path.dist + config.path.assets + config.path.fonts))
 }
 
 // WATCH -------------------------------------------------------------
 
 function watch__assets () {
-  watch(assets__images__src + '**/*.{png,jpg,jpeg,gif}', series(images, reload))
-  watch(assets__icons__src + '**/*.svg', series(icons, reload))
-  watch(assets__favicons__src + '**/favicon_src.png', series(favicons, reload))
-  watch(assets__fonts__src + '**/*.{woff,woff2}', series(fonts, reload))
+  watch(config.path.src + config.path.resources + config.path.assets + config.path.images + '**/*.{png,jpg,jpeg,gif}', series(images, reload))
+  watch(config.path.src + config.path.resources + config.path.assets + config.path.icons + '**/*.svg', series(icons, reload))
+  watch(config.path.src + config.path.resources + config.path.assets + config.path.favicons + '**/favicon_src.png', series(favicons, reload))
+  watch(config.path.src + config.path.resources + config.path.assets + config.path.fonts + '**/*.{woff,woff2}', series(fonts, reload))
 }
 
 // COMPOSITION -------------------------------------------------------------
@@ -285,28 +258,25 @@ const fonts = series(clean__fonts, copy__fonts)
 // SCRIPT
 ////////////////////////////////////////////////////////////////////////////////
 
-const scripts__src = (root_src + path.resources).replace('//', '/')
-const scripts__dest = (root_dist).replace('//', '/')
-
 // CLEAN -------------------------------------------------------------
 
-function clean__scripts__main () { return del(scripts__dest + 'main.min.{js,js.map}') }
+function clean__scripts__main () { return del(config.path.dist + 'main.min.{js,js.map}') }
 
 // PROCESS -------------------------------------------------------------
 
 function process__scripts__main () {
-  return src([scripts__src + 'main.js', snippets__src + '**/script.js'], { sourcemaps: !ENV === 'production' ? (!ENV === 'staging' ? true : false) : false })
+  return src([config.path.src + config.path.resources + 'main.js', config.path.src + config.path.snippets + '**/script.js'], { sourcemaps: !ENV === 'production' ? (!ENV === 'staging' ? true : false) : false })
     .pipe(gulpif(DEBUG, debug({ title: '## MAIN:' })))
     .pipe(concat('main.js'))
     .pipe(gulpif((ENV === 'production' || ENV === 'staging'), uglify()))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(dest(scripts__dest, { sourcemaps: !ENV === 'production' ? (!ENV === 'staging' ? '.' : false) : false }))
+    .pipe(dest(config.path.dist, { sourcemaps: !ENV === 'production' ? (!ENV === 'staging' ? '.' : false) : false }))
 }
 
 // WATCH -------------------------------------------------------------
 
 function watch__scripts () {
-  watch([scripts__src + 'main.js', snippets__src + '**/script.js'], series(scripts__main, reload))
+  watch([config.path.src + config.path.resources + 'main.js', config.path.src + config.path.snippets + '**/script.js'], series(scripts__main, reload))
 };
 
 // COMPOSITION -------------------------------------------------------------
@@ -317,29 +287,26 @@ const scripts__main = series(clean__scripts__main, process__scripts__main)
 // STYLE
 ////////////////////////////////////////////////////////////////////////////////
 
-const styles__src = (root_src + resources).replace('//', '/')
-const styles__dest = (root_dist).replace('//', '/')
-
 // CLEAN -------------------------------------------------------------
 
-function clean__styles () { return del(styles__dest + '*.min.{css,css.map}') }
+function clean__styles () { return del(config.path.dist + '*.min.{css,css.map}') }
 
 // PROCESS -------------------------------------------------------------
 
 function process__styles () {
   scss.compiler = sass
-  return src(styles__src + 'main.scss', { sourcemaps: !ENV === 'production' ? (!ENV === 'staging' ? true : false) : false })
+  return src(config.path.src + config.path.resources + 'main.scss', { sourcemaps: !ENV === 'production' ? (!ENV === 'staging' ? true : false) : false })
     .pipe(gulpif(DEBUG, debug({ title: '## STYLE:' })))
     .pipe(scss({ outputStyle: ENV === 'production' ? (ENV === 'staging' ? 'compressed' : 'expanded') : 'expanded' }).on('error', scss.logError))
     .pipe(autoprefixer()).pipe(rename({ suffix: '.min' }))
-    .pipe(dest(styles__dest, { sourcemaps: !ENV === 'production' ? (!ENV === 'staging' ? '.' : false) : false }))
+    .pipe(dest(config.path.dist, { sourcemaps: !ENV === 'production' ? (!ENV === 'staging' ? '.' : false) : false }))
 }
 
 // WATCH -------------------------------------------------------------
 
 function watch__styles () {
-  watch(snippets__src + '**/*.scss', series(styles, reload))
-  watch(styles__src + '**/*.scss', series(styles, reload))
+  watch(config.path.src + config.path.snippets + '**/*.scss', series(styles, reload))
+  watch(config.path.src + config.path.resources + '**/*.scss', series(styles, reload))
 }
 
 // COMPOSITION -------------------------------------------------------------
@@ -367,11 +334,3 @@ if (ENV === 'production' || ENV === 'staging') {
 ////////////////////////////////////////////////////////////////////////////////
 
 task('clear', () => cache.clearAll())
-
-function prep (paths) {
-  for (var p in paths) {
-    var value = paths[p]
-    paths[p] = value.length ? value.replace(/\/?$/, '/') : value
-  }
-  return paths
-}
