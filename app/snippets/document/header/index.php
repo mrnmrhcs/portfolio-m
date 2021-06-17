07cdbbb7192260e4754a65f1e43c4acf59b3d745
mrnmrhcs <m.service@kaimberg.com>
2021-06-17 02:42:30 +0200

    update(favicons)

--- app/snippets/document/header/index.php
+++ app/snippets/document/header/index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <title>Portfolio — Marian Schramm</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="Marian Schramm">
+    <meta name="description" content="Portfolio Website">
     <meta name="language" content="en">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Marian Schramm">
@@ -17,8 +17,8 @@
     <link rel="icon" type="image/png" sizes="48x48" href="../../../assets/favicons/favicon-48x48.png">
     <link rel="manifest" href="../../../assets/favicons/manifest.json">
     <meta name="mobile-web-app-capable" content="yes">
-    <meta name="theme-color" content="#fff">
-    <meta name="application-name" content="Portfolio M">
+    <meta name="theme-color" content="rgba(164,170,204,0)">
+    <meta name="application-name" content="Portfolio — Marian Schramm">
     <link rel="apple-touch-icon" sizes="57x57" href="../../../assets/favicons/apple-touch-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="../../../assets/favicons/apple-touch-icon-60x60.png">
     <link rel="apple-touch-icon" sizes="72x72" href="../../../assets/favicons/apple-touch-icon-72x72.png">
@@ -32,7 +32,7 @@
     <link rel="apple-touch-icon" sizes="1024x1024" href="../../../assets/favicons/apple-touch-icon-1024x1024.png">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    <meta name="apple-mobile-web-app-title" content="portfolio-m">
+    <meta name="apple-mobile-web-app-title" content="Portfolio-M">
     <link rel="apple-touch-startup-image" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="../../../assets/favicons/apple-touch-startup-image-640x1136.png">
     <link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="../../../assets/favicons/apple-touch-startup-image-750x1334.png">
     <link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="../../../assets/favicons/apple-touch-startup-image-828x1792.png">
@@ -56,7 +56,7 @@
     <link rel="apple-touch-startup-image" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" href="../../../assets/favicons/apple-touch-startup-image-2732x2048.png">
     <link rel="apple-touch-startup-image" media="(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" href="../../../assets/favicons/apple-touch-startup-image-2160x1620.png">
     <link rel="icon" type="image/png" sizes="228x228" href="../../../assets/favicons/coast-228x228.png">
-    <meta name="msapplication-TileColor" content="#FFFFFF">
+    <meta name="msapplication-TileColor" content="rgba(164,170,204,0)">
     <meta name="msapplication-TileImage" content="../../../assets/favicons/mstile-144x144.png">
     <meta name="msapplication-config" content="../../../assets/favicons/browserconfig.xml">
     <link rel="yandex-tableau-widget" href="../../../assets/favicons/yandex-browser-manifest.json">
