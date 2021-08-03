29f86884bf03c81e4f853e24130d5d7be0c429fe
mrnmrhcs <m.service@kaimberg.com>
2021-08-03 11:24:08 +0200

    update(content)

--- app/templates/about.php
+++ app/templates/about.php
@@ -6,7 +6,7 @@
       <header class="about__header context-content">
         <div class="container">
           <h1 class="about__title">About</h1>
-          <p class="about__intro">Frontend developer based in Berlin with great enthusiasm for all design and technology related aspects of digital ecosystems. I have worked in design and web development agencies and contributed in a variety of successful digital design projects.</p>
+          <p class="about__intro">Frontend Web Developer based in Berlin. I contributed to a variety of successful digital design and web development projects.</p>
         </div>
       </header>
       <main class="about__main">
@@ -16,19 +16,19 @@
               <div class="about__department">
                 <h3>development</h3>
                 <ul class="list list_skills">
-                  <li class="list__item">HTML5, CSS/SCSS, PHP/Twig Integration</li>
-                  <li class="list__item">JavaScript/ES6+/jQuery, SPA</li>
-                  <li class="list__item">ESLint, Stylelint, PHPCS, BEM, Emmet</li>
-                  <li class="list__item">CMS Kirby, Wordpress/Timber</li>
+                  <li class="list__item">HTML5, CSS/SCSS, PHP/Twig, JavaScript/ES6</li>
+                  <li class="list__item">React/Redux, Web Components</li>
+                  <li class="list__item">ESLint, Stylelint, PHPCS, BEM</li>
+                  <li class="list__item">CMS Wordpress, Kirby</li>
                 </ul>
               </div>
               <div class="about__department">
-                <h3>design</h3>
+                <h3>concept</h3>
                 <ul class="list list_skills">
-                  <li class="list__item">Node.js, NPM, Composer, Gulp, Webpack</li>
-                  <li class="list__item">Git Version Control, Asset Optimization</li>
-                  <li class="list__item">Usability, User Experience, Accessibility</li>
-                  <li class="list__item">User Interface Design</li>
+                  <li class="list__item">NPM, Composer, Webpack, Gulp</li>
+                  <li class="list__item">Git Version Control, PowerShell</li>
+                  <li class="list__item">UX/UI Design, Usability, Accessibility</li>
+                  <li class="list__item">Asset Optimization</li>
                 </ul>
               </div>
             </div>
