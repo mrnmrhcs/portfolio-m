59bb7e0cf32b283e7368dde8cc49badec9a8ca2e
mrnmrhcs <m.service@kaimberg.com>
2021-08-17 05:41:29 +0200

    update(content)

--- app/templates/about.php
+++ app/templates/about.php
@@ -6,7 +6,7 @@
       <header class="about__header context-content">
         <div class="container">
           <h1 class="about__title">About</h1>
-          <p class="about__intro">Frontend Web Developer based in Berlin. I work for digital agencies and contributed to a variety of successful digital design and web development projects.</p>
+          <p class="about__intro">Frontend Web Developer based in Berlin, working for digital agencies and contributed to a variety of successful digital design and web development projects.</p>
         </div>
       </header>
       <main class="about__main">
@@ -14,20 +14,20 @@
           <div class="container">
             <div class="about__departments">
               <div class="about__department">
-                <h3>development</h3>
+                <h3>design</h3>
                 <ul class="list list_skills">
-                  <li class="list__item">HTML5, CSS/SCSS, PHP/Twig, JavaScript/ES6</li>
-                  <li class="list__item">React/Redux, Web Components</li>
-                  <li class="list__item">ESLint, Stylelint, PHPCS, BEM</li>
-                  <li class="list__item">CMS Wordpress, Kirby</li>
+                  <li class="list__item">HTML/CSS, PHP/Twig, SCSS/BEM</li>
+                  <li class="list__item">JavaScript, React, Web Components</li>
+                  <li class="list__item">UX/UI, Usability, Accessibility</li>
+                  <li class="list__item">Wordpress, Kirby</li>
                 </ul>
               </div>
               <div class="about__department">
-                <h3>concept</h3>
+                <h3>development</h3>
                 <ul class="list list_skills">
                   <li class="list__item">NPM, Composer, Webpack, Gulp</li>
+                  <li class="list__item">ESLint, Stylelint, PHPCS</li>
                   <li class="list__item">Git Version Control, PowerShell</li>
-                  <li class="list__item">UX/UI Design, Usability, Accessibility</li>
                   <li class="list__item">Asset Optimization</li>
                 </ul>
               </div>
